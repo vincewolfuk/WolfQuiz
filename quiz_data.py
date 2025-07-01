e5c3883c795c9d45d6c5dc76f8f41813ac7c614f
Vicente Lopez <vicente.ld83@gmail.com>
2025-07-01 16:04:47 +0100

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,26 @@
+quiz = [
+    {
+        "question": "Which country is this capital city from?",
+        "image": "paris.jpg",
+        "options": ["Germany", "France", "Italy", "Spain"],
+        "answer": "France",
+        "feedback_image": "france.jpg"
+    },
+    {
+        "question": "Which country is this capital city from?",
+        "image": "tokyo.jpg",
+        "options": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan",
+        "feedback_image": "japan.jpg"
+    },
+    {
+        "question": "Which country is this capital city from?",
+        "image": "caracas.jpg",
+        "options": ["Chile", "España", "Honduras", "Venezuela"],
+        "answer": "Venezuela",
+        "feedback_image": "Venezuela.jpg"
+    },
+    # Add 8 more...
+]
+
+
